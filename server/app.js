1fb64149d0fbae0524d44cc3510c55e75134918e
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-07-01 22:23:06 +0530

    updated furnispace files

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Welcome to FurniSpace");
+});
+
+module.exports = app;
