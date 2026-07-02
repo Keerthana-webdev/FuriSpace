5cfb0b550457028353a103bcdde451207b0c9d80
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-07-02 19:57:04 +0530

    updated furnispace files

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+{
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    role:{
+        type:String,
+        enum:["customer","admin"],
+        default:"customer"
+    },
+
+    profileImage:{
+        type:String,
+        default:""
+    },
+
+    phone:{
+        type:String
+    },
+
+    address:{
+        type:String
+    }
+
+},
+{
+    timestamps:true
+}
+);
+
+module.exports = mongoose.model("User",userSchema);
