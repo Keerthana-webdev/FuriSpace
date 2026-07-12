8c099ddae7dd0ec1b48580a1f6caa799db339bae
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-07-12 19:08:27 +0530

    updated furnispace files

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
 
-const { addToCart } = require("../controllers/cartController");
+const { addToCart, getCart, updateCartItem } = require("../controllers/cartController");
 
 router.post("/", protect, addToCart);
+router.get("/", protect, getCart);
+router.put("/:productId", protect, updateCartItem);
 
 module.exports = router;
