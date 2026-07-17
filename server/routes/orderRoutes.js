e938f05eb2dcca53701dd46cf62ebb7ff127a101
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-07-17 07:25:06 +0530

    updated furnispace files

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -7,6 +7,6 @@ const { placeOrder, getMyOrders, getOrderById } = require("../controllers/orderC
 
 router.post("/", protect, placeOrder);
 router.get("/my-orders", protect, getMyOrders);
-router.get("/my-orders/:orderId", protect, getOrderById);
+router.get("/:orderId", protect, getOrderById);
 
 module.exports = router;
