d308b57e0b54476d57da6d09993bca5ce9c85085
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-08-10 20:38:14 +0530

    updated furnispace files

--- client/src/components/product/ProductBanner.jsx
+++ client/src/components/product/ProductBanner.jsx
@@ -3,13 +3,21 @@ import "./ProductBanner.css";
 function ProductBanner() {
   return (
     <section className="product-banner">
-      <div className="overlay">
-        <h1>Premium Furniture Collection</h1>
+      <div className="product-banner-overlay">
+        <div className="product-banner-content">
+          <p className="banner-small-text">FURNISPACE COLLECTION</p>
 
-        <p>
-          Discover beautifully crafted furniture for every
-          room in your home.
-        </p>
+          <h1>
+            Premium Furniture
+            <br />
+            For Modern Living
+          </h1>
+
+          <p>
+            Discover beautifully crafted furniture designed to make your home
+            comfortable and stylish.
+          </p>
+        </div>
       </div>
     </section>
   );
