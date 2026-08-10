7f6a34c3f88622d0c98ad2154fb5af7a189211f8
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-08-10 20:39:47 +0530

    updated furnispace files

--- client/src/components/product/ProductSearch.jsx
+++ client/src/components/product/ProductSearch.jsx
@@ -1,19 +1,24 @@
-import { FiSearch } from "react-icons/fi";
+import { FiSearch, FiX } from "react-icons/fi";
+
 import "./ProductSearch.css";
 
 function ProductSearch({ search, setSearch }) {
   return (
     <div className="product-search">
-      <FiSearch />
+      <FiSearch className="search-icon" />
 
       <input
         type="text"
-        placeholder="Search furniture..."
+        placeholder="Search sofas, chairs, tables..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
 
-      {search && <button onClick={() => setSearch("")}>Clear</button>}
+      {search && (
+        <button className="clear-search" onClick={() => setSearch("")}>
+          <FiX />
+        </button>
+      )}
     </div>
   );
 }
