79a5349182d032f871751a7b8d8a14af6a9d34d9
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-05 20:12:53 +0530

    updated furnispace files

--- client/src/pages/AdminOrders/AdminOrders.jsx
+++ client/src/pages/AdminOrders/AdminOrders.jsx
@@ -135,10 +135,6 @@ function AdminOrders() {
     0,
   );
 
-  // ==========================================
-  // UI
-  // ==========================================
-
   return (
     <main className="admin-orders-page">
       <div className="admin-orders-container">
