5eb2e193cb21c0fb33f39a4d8f9540a91324aaa3
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-08 21:10:03 +0530

    updated furnispace files

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ const connectDB = require("./config/db");
 
 connectDB();
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
