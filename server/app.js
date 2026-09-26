b9b2da3099aba0e93fcb0b80638c0a305318b9c4
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-26 16:21:34 +0530

    updated furnispace file

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const authRoutes = require("./routes/authRoutes");
+const adminRoutes = require("./routes/adminRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+// ROUTES
+app.use("/api/auth", authRoutes);
+
+app.use("/api/admin", adminRoutes);
+
+app.use("/api/products", productRoutes);
+
+app.use("/api/cart", cartRoutes);
+
+app.use("/api/orders", orderRoutes);
+
+// ROOT TEST
+app.get("/", (req, res) => {
+  res.send("Welcome to FurniSpace Backend");
+});
+
+module.exports = app;
