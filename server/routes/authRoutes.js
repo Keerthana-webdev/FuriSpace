065f1f5a7aceab924689fc8502a4b0d8ab8ffd55
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-26 16:31:24 +0530

    updated furnispace file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,21 +10,33 @@ const {
 
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
+// REGISTER
+
 router.post("/register", registerUser);
+
+// LOGIN
 
 router.post("/login", loginUser);
 
+// PROFILE
+
 router.get("/profile", protect, getProfile);
 
-// ADMIN TEST ROUTE
+// ADMIN TEST
+
 router.get("/admin-test", protect, adminOnly, (req, res) => {
   res.status(200).json({
     success: true,
+
     message: "Admin access verified successfully.",
+
     user: {
       id: req.user._id,
+
       name: req.user.name,
+
       email: req.user.email,
+
       role: req.user.role,
     },
   });
