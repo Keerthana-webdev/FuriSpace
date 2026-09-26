39ee3e575cc39cb09670096372634fdf0722b5ae
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-09-26 16:20:04 +0530

    updated furnispace file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const app = require("./app");
@@ -6,11 +7,8 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
